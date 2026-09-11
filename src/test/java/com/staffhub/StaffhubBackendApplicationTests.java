0497b7c0c9dcc606eeedd589d61d343b705b0f58
IT25100511 <It25100511@my.sliit.lk>
2026-09-12 01:22:16 +0530

    second commit

---
+++ src/test/java/com/staffhub/StaffhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
