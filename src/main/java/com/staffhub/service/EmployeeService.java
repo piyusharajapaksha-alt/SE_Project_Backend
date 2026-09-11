f08ed18dd810d09968c42b75e3759cbcd5e39802
IT25100511 <It25100511@my.sliit.lk>
2026-09-12 02:12:18 +0530

    feat: add employee service

---
+++ src/main/java/com/staffhub/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.staffhub.service;
+
+import com.staffhub.model.Employee;
+import com.staffhub.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    // Get all employees
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
